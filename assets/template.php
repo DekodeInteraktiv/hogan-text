b2eaafe7f15960e48fac08e485497c780300fe33
Stian <stian@dekode.no>
2018-03-23 14:39:27 +0100

    Remove wp_kses_post for oEmbed support (#6)

--- assets/template.php
+++ assets/template.php
@@ -17,4 +17,4 @@ if ( ! defined( 'ABSPATH' ) || ! ( $this instanceof Text ) ) {
 	return; // Exit if accessed directly.
 }
 
-echo wp_kses_post( $this->content );
+echo $this->content; // WPCS: XSS OK.
